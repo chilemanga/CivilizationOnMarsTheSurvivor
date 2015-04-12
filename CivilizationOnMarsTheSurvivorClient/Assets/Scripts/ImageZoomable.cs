3ea3159548a14fa3752e2ff60f61c9afd3de3e47
Hernan Espinosa <hernan.espinosa@dena.com>
2015-04-12 10:07:55 -0300

    Added mission briefing scene.

---
+++ CivilizationOnMarsTheSurvivorClient/Assets/Scripts/ImageZoomable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageZoomable : MonoBehaviour {
+
+	private float timeElapsed;
+	public float timeToGoNextScene = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		transform.localScale += Vector3.one * Time.deltaTime/timeToGoNextScene;
+
+		if( timeElapsed > timeToGoNextScene ) {
+			Application.LoadLevel(3);
+		}
+	}
+}
