09b24a333a6c532da59601093927af4021f47ffc
Osvaldo Valdenegro <osvaldo.valdenegro@gmail.com>
2015-04-12 03:21:24 -0300

    Menu principal, primera version.

---
+++ CivilizationOnMarsTheSurvivorClient/Assets/Scripts/xplorer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class xplorer : MonoBehaviour {
+
+	public GameObject InformativePanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void onXplorerClick(int buttonIndex){
+		Debug.Log (string.Format ("hizo clic a {0} {1}", buttonIndex, "caca") );
+
+		switch(buttonIndex)
+		{
+		
+		case 0: 
+			Debug.Log("facil");
+			break;
+		case 1:
+			Debug.Log("medio");
+			break;
+		case 2:
+			Debug.Log("dificil");
+			break;
+		default:
+			Debug.Log("nada");
+			break;
+		}
+		InformativePanel.SetActive (true);
+	}
+	public void ExitInformativa(){
+		Debug.Log ("murio");
+		InformativePanel.SetActive (false);
+	}
+}
